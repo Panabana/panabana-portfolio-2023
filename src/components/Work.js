6f9c3e2897f792e875a2e9c2ddf9abf4ba26aceb
Alexander Bengtsson <palexanderbengtsson@gmail.com>
2023-04-24 18:39:31 +0200

    fixed missing return in Work.js + skill key

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,1 +1,14 @@
-export default function Work() {}
+import { UsersIcon } from "@heroicons/react/solid";
+
+export default function Work() {
+  return (
+    <section id="work">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <UsersIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Work testimonials
+        </h1>
+      </div>      
+    </section>
+  )
+}
